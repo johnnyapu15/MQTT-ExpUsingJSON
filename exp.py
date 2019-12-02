191a35a701e58e6b02f0f1ddbbffb7c4ddc5a15d
johnnyapu15 <johnnyapu15@gmail.com>
2019-12-02 22:13:30 +0900

    c

--- exp.py
+++ exp.py
@@ -28,6 +28,8 @@ def exp(filename, expName = "test", velo = 0):
             e[d]['messageId'] = expName
         if (e[topic] == 'Test-mW'):
             e[d]['value2'] = velo
+        if (e[topic] == 'Test-Target'):
+            e[d]['value'] = velo
         if t >= e[ts]:
             # play
             mqttc.publish(e[topic], str(e[d]))
@@ -36,12 +38,14 @@ def exp(filename, expName = "test", velo = 0):
             # play
             mqttc.publish(e[topic], str(e[d]))
         t = e[ts]
-def exp2(test):
+def exp2(test, sub = None):
     global mqttc
     mqttc = mqtt.Client()
     mqttc.connect(brok, port)
     #kin-A-2
-    testVelo = 4.2 # 4.2m/s ~= 15km/h
+    # 19km/h = 5.3m/s
+
+    testVelo = 5.3 # 4.2m/s ~= 15km/h
     d = test.split('-')
     building = d[0]
     exp(building + '-init',None,testVelo)
@@ -49,5 +53,42 @@ def exp2(test):
     for i, e in enumerate(d[1:]):
         exp(building + '-' + e,None,testVelo)
         statName += '-' + e
+    if sub != None:
+        statName += sub
     exp('Fire', statName)
+
+def t(f):
+    global mqttc
+    mqttc = mqtt.Client()
+    mqttc.connect(brok, port)
+    exp('settarget',None, f)
 #exp2('kin-A-1')
+
+
+
+# t(1)
+# exp2('etri-A-1','first')
+# input()
+# exp2('etri-A-2','first')
+# input()
+# exp2('etri-A-3','first')
+# input()
+# exp2('etri-B-1','first')
+# input()
+# exp2('etri-B-2','first')
+# input()
+# exp2('etri-B-3','first')
+# input()
+# t(6)
+# exp2('etri-A-1','last')
+# input()
+# exp2('etri-A-2','last')
+# input()
+# exp2('etri-A-3','last')
+# input()
+# exp2('etri-B-1','last')
+# input()
+# exp2('etri-B-2','last')
+# input()
+# exp2('etri-B-3','last')
+# input()
